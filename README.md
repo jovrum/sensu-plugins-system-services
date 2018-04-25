f2bfb9eb7077bc23550813c4c3ba7cbf698aa170
Jakob Øvrum <jovrum@cisco.com>
2018-04-25 15:49:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## sensu-plugins-system-services
+
+[![Build Status](https://travis-ci.org/jovrum/sensu-plugins-system-services.svg?branch=master)](https://travis-ci.org/jovrum/sensu-plugins-system-services)
+[![Gem Version](https://badge.fury.io/rb/sensu-plugins-system-services.svg)](http://badge.fury.io/rb/sensu-plugins-system-services)
+[![Dependency Status](https://gemnasium.com/jovrum/sensu-plugins-system-services.svg)](https://gemnasium.com/jovrum/sensu-plugins-system-services)
+[![Community Slack](https://slack.sensu.io/badge.svg)](https://slack.sensu.io/badge)
+
+## Functionality
+
+## Files
+
+## Usage
+
+## Installation
+
+[Installation and Setup](http://sensu-plugins.io/docs/installation_instructions.html)
+
+## Notes
---
+++ bin/check-system-services-systemd.rb
@@ -0,0 +1,60 @@
+#! /usr/bin/env ruby
+# encoding: UTF-8
+#
+# check-system-services-systemd
+#
+# DESCRIPTION:
+# Check status of system services.
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin, ruby-dbus
+#
+# USAGE:
+#
+# NOTES:
+#
+
+require 'sensu-plugin/check/cli'
+require 'dbus'
+
+class CheckSystemServicesSystemd < Sensu::Plugin::Check::CLI
+  option :critical_services,
+         short: '-c SERVICES',
+         long: '--critical-services SERVICES',
+         description: 'issues with any of these comma-separated services are critical events',
+         default: ''
+
+  def run
+    dbus = DBus::SystemBus.instance
+    service = dbus.service('org.freedesktop.systemd1')
+    object = service.object('/org/freedesktop/systemd1')
+    object.introspect
+
+    critical_services = config[:critical_services]
+      .split(',')
+      .map { |name| name.include?('.') ? name : "#{name}.service" }
+      .to_set
+
+    failing_units = object.ListUnits.first
+      .select { |name, desc, load_state, active_state| active_state == 'failed' }
+      .map { |name, desc, load_state, active_state| name }
+      .to_set
+
+    if failing_units.empty?
+      ok
+    else
+      output = "services are failing: #{failing_units.to_a.sort.join(', ')}"
+      if failing_units.intersect?(critical_services)
+        critical(output)
+      else
+        warning(output)
+      end
+    end
+  end
+end
